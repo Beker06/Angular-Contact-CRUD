deecb4185492a4be5aba4790b34aedb7b70b19f0
Eliam Vazquez <98252478+Beker06@users.noreply.github.com>
2024-09-13 02:41:47 -0700

    [V1.0.0]

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,9 @@
+export const APP_CONTACTS = {
+    COLLECTION_NAME: 'contacts',
+    MESSAGES : {
+        CONTACT_UPDATED: 'Contact updated succesfully',
+        CONTACT_ADDED: 'Contact added sucessfully',
+        CONTACT_DELETED: 'Contact deleted sucessfully',
+        CONFIRMATION_PROMPT: 'Are you sure you want to delete this contact?'
+    }
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+        "projectId":"angular-contact-25500",
+        "appId":"1:976917731625:web:834abac3eefc994c503f38",
+        "storageBucket":"angular-contact-25500.appspot.com",
+        "apiKey":"AIzaSyC2zQtS0RDc8qkpbgS73cd78q4CJCmThn4",
+        "authDomain":"angular-contact-25500.firebaseapp.com",
+        "messagingSenderId":"976917731625"
+    }
+};
